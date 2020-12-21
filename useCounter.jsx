e94d219c8031270160b21907c6e9222ee094f246
Jose Luis Flores Fuentes <floresfuentes93@gmail.com>
2020-12-21 11:07:59 -0600

    commit 1

---
+++ useCounter.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+export const useCounter = (initialState = 1) => {
+    const [count, setCount] = useState(initialState)
+
+    const increment = () => setCount(count+1)
+
+    const decrement = () => setCount(count-1)
+
+    const reset = () => setCount(initialState)
+
+    return {count, increment, decrement, reset}
+
+}
